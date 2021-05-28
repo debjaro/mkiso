17554ea5301ec9b5e0289c2881b70b4114584001
paledega <38860643+paledega@users.noreply.github.com>
2021-05-28 08:37:13 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,17 @@ if [[ ! $UID -eq 0 ]] ; then
     echo -e "\033[31;1mYou must be root!\033[:0m"
     exit 1
 fi
+#### Remove all environmental variable
+for e in $(env | sed "s/=.*//g") ; do
+    unset "$e" &>/dev/null
+done
+
+#### Set environmental variables
+export PATH=/bin:/usr/lib:/sbin:/usr/sbin
+export LANG=C
+export SHELL=/bin/bash
+export TERM=linux
+
 #### Install dependencies
 if which apt &>/dev/null && [[ -d /var/lib/dpkg && -d /etc/apt ]] ; then
     apt-get update
